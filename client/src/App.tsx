da7fe572f49332e0317ccb76078b22404840e7cc
n-andr <150958639+n-andr@users.noreply.github.com>
2026-01-13 22:59:53 +0100

    add: server and client folders and react + vite + ts setup for client

---
+++ client/src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <main style={{ padding: 24, fontFamily: "system-ui" }}>
+      <h1>You draw me crazy</h1>
+      <p>App in progress... </p>
+    </main>
+  );
+}
