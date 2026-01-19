8793e892c518ebdf96159ac029c7abf32cbe140b
lotte-detaeye <lieselot.detaeye@gmail.com>
2026-01-19 16:35:10 +0100

    add: base Button component and base sandbox page

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -6,9 +6,16 @@
 /*   By: lde-taey <lde-taey@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/14 16:17:58 by lde-taey          #+#    #+#             */
-/*   Updated: 2026/01/14 16:18:06 by lde-taey         ###   ########.fr       */
+/*   Updated: 2026/01/16 15:11:37 by lde-taey         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+import { Module } from '@nestjs/common';
+import { ConfigModule } from './config/config.module';
+import { DatabaseModule } from './database/database.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { GameModule } from './game/game.module';
 
 @Module
 ({
@@ -19,5 +26,7 @@
     UsersModule,
     GameModule,
   ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
