86dfcba3ffdeef6fc66050fc947c7d5fd66c4d51
Nick Boer <nboer@c1b1c1.42berlin.de>
2026-01-25 16:33:51 +0100

    add: auth controller login and signup

--- requirements/backend/src/auth/auth.controller.ts
+++ requirements/backend/src/auth/auth.controller.ts
@@ -6,29 +6,33 @@
 /*   By: nboer <nboer@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/24 18:54:09 by nboer             #+#    #+#             */
-/*   Updated: 2026/01/24 20:13:07 by nboer            ###   ########.fr       */
+/*   Updated: 2026/01/25 16:04:59 by nboer            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 import { Body, Controller, Get, Post } from '@nestjs/common'
+import { AuthService } from './auth.service';
 import { SignupDto } from './dto/signup.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
-
+	constructor(private readonly authService: AuthService) {}
+	
 	@Get()
 	getAuth() {
 		return 'Auth';
 	}
 
 	@Post('signup')
-	signup(@Body() dto: SignupDto) {
-		
-		return 'Signup endpoint';
+	async signup(@Body() dto: SignupDto) {
+		console.log(dto); // for debugging with "docker logs backend"
+		return this.authService.signup(dto.email, dto.password, dto.username);
 	}
 
 	@Post('login')
-	login() {
-		return 'Login endpoint';
+	async login(@Body() dto: LoginDto) {
+		console.log(dto); // for debugging "docker logs backend"
+		return this.authService.login(dto.username, dto.password);
 }
 }
--- requirements/backend/src/auth/dto/login.dto.ts
+++ requirements/backend/src/auth/dto/login.dto.ts
@@ -6,7 +6,18 @@
 /*   By: nboer <nboer@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/24 19:09:06 by nboer             #+#    #+#             */
-/*   Updated: 2026/01/24 19:09:07 by nboer            ###   ########.fr       */
+/*   Updated: 2026/01/25 15:51:55 by nboer            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+import { IsString, MinLength, MaxLength } from 'class-validator';
+
+export class LoginDto {
+	@IsString()
+	password: string;
+
+	@IsString()
+	@MinLength(2)
+	@MaxLength(20)
+	username: string;
+}
