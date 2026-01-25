6dd5281a655417e07e146384a72d68bfc95958d7
steffi <steffi.bap@gmail.com>
2026-01-25 15:19:45 +0100

    add: dummy database service + prisma folder

--- requirements/backend/src/users/users.controller.ts
+++ requirements/backend/src/users/users.controller.ts
@@ -1,9 +1,13 @@
 import { Controller, Get, Post, Body, Param } from "@nestjs/common";
 import { UsersService } from "./users.service";
+import { DatabaseService } from "../database/database.service";
 
 @Controller('users')
 export class UsersController {
-	constructor(private readonly usersService: UsersService) {}
+	constructor(
+		private readonly usersService: UsersService,
+		private readonly databaseService: DatabaseService,
+	) {}
 
 	@Post('create')
 	createUser(
@@ -14,8 +18,17 @@ export class UsersController {
 		return this.usersService.createUser(nickname, email, password);
 	}
 
+	@Get('test-db')
+	async testDb() {
+	const result = await this.databaseService.query('SELECT 1+1 AS sum');
+	console.log(result.rows);
+	return result.rows;
+	}
+	
 	@Get(':nickname')
 	getUser(@Param('nickname') nickname: string) {
 		return this.usersService.getUser(nickname);
 	}
+
+	
 }
