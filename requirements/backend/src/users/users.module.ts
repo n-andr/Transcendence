3ee569d24f51f488788450e653bbb603cb6f507a
steffi <steffi.bap@gmail.com>
2026-01-25 17:06:33 +0100

    add: providers PrismaService

--- requirements/backend/src/users/users.module.ts
+++ requirements/backend/src/users/users.module.ts
@@ -3,10 +3,11 @@ import { UsersService } from "./users.service";
 import { UsersController } from "./users.controller";
 import { DatabaseModule } from "src/database/database.module";
 import { PrismaModule } from "src/database/prisma.module";
+import { PrismaService } from "src/database/prisma.service";
 
 @Module({
 	controllers: [UsersController],
-	providers: [UsersService],
+	providers: [UsersService, PrismaService],
 	exports: [UsersService],
 	imports: [DatabaseModule, PrismaModule],
 })
