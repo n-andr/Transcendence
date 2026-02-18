e153715e7aad03d952e43c22892676a9c12aff56
n-andr <150958639+n-andr@users.noreply.github.com>
2026-02-18 01:59:24 +0100

    add: set up for svg dravwing

--- requirements/frontend/src/components/room/DrawerPanel.tsx
+++ requirements/frontend/src/components/room/DrawerPanel.tsx
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 export default function DrawerPanel(){ return(<div></div>);}
+=======
+export default function DrawerPanel(){ return;}
+>>>>>>> 8677b65 (add: set up for svg dravwing)
--- requirements/frontend/src/components/room/GuesserPanel.tsx
+++ requirements/frontend/src/components/room/GuesserPanel.tsx
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 export default function GuesserPanel(){ return (<div></div>);}
+=======
+export default function GuesserPanel(){}
+>>>>>>> 8677b65 (add: set up for svg dravwing)
