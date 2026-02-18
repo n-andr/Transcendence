0d34e02f4b1393ab51cba3ee0105f6cf9d8e628f
n-andr <150958639+n-andr@users.noreply.github.com>
2026-02-18 21:22:12 +0100

    sync: WIP local storage and drawing wip

--- requirements/frontend/src/components/room/drawingBoard.tsx
+++ requirements/frontend/src/components/room/drawingBoard.tsx
@@ -4,7 +4,7 @@ import { useEffect, useMemo, useRef, useState } from "react";
 import { socket } from "../../api/socket";
 import { ChatMessageRow, type ChatMessage } from "./chatMessageRow";
 import { mockMessages } from "./chat.mock";
-import SvgBoard from "../../features/drawing/SvgBoard";
+// import SvgBoard from "../../features/drawing/SvgBoard";
 import DrawerPanel from "./DrawerPanel";
 import GuesserPanel from "./GuesserPanel";
 import { useSessionStore } from "../../state/sessionStore";
@@ -20,9 +20,9 @@ export default function DrawingBoard({ onGuessCorrect }: Props) {
   const [messages, setMessages] = useState<ChatMessage[]>(mockMessages);
   const scrollAnchorRef = useRef<HTMLDivElement | null>(null);
   const currentUserId = useSessionStore((s: any) => s.userId)
-  const roomId = useSessionStore((s: any) => s.roomId)
+  const roomId = useSessionStore((s:any) => s.roomId)
 
-  const role = useSessionStore((s: any) => s.role);
+  const role = useSessionStore((s:any) => s.role);
 
   const isDrawer = role === "drawer";
 
@@ -83,18 +83,20 @@ export default function DrawingBoard({ onGuessCorrect }: Props) {
     };
   }, [onGuessCorrect]);
 
+  const session = useSessionStore();
+	console.log(session);
   return (
     <div className="flex flex-col lg:flex-row gap-4 flex-1 min-h-0">
       {/* Canvas area */}
       <div className="relative bg-surface border border-gray-400 rounded-lg flex-1 min-h-[280px] lg:min-h-0">
-        {/* <canvas
+        <canvas
           className="w-full h-full rounded cursor-crosshair"
           width={1600}
           height={1200}
-        /> */}
-		<SvgBoard roomId={roomId} socket={socket} mode={isDrawer ? "draw" : "view"} />
+        />
+		{/* <SvgBoard roomId={roomId} socket={socket} mode={isDrawer ? "draw" : "view"} /> */}
 		{/* tools panel */}
-        {isDrawer ? <DrawerPanel /> : <GuesserPanel />}
+        {/* {isDrawer ? <DrawerPanel /> : <GuesserPanel />} */}
       </div>
 
 
--- requirements/frontend/src/features/drawing/types.ts
+++ requirements/frontend/src/features/drawing/types.ts
@@ -1,12 +1,1 @@
 // types.ts
-import type { StrokeId } from "./protocol";
-
-export type Stroke = {
-  id: StrokeId;
-  points: Array<[number, number]>; // normalized
-};
-
-export type DrawingState = {
-  strokes: Record<StrokeId, Stroke>;
-  order: StrokeId[]; // render order
-};
