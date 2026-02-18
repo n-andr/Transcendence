dcb8711cd036b6c3ff35b5084279c77f7af582cf
steffi <steffi.bap@gmail.com>
2026-02-18 10:51:42 +0100

    add: WSS DTOs

---
+++ requirements/shared/ws.events.ts
@@ -0,0 +1,15 @@
+export const WS_EVENTS = {
+	JOIN_ROOM: "joinRoom",
+	ROOM_STATE: "roomState",
+	GUESS: "guess",
+	GUESS_UPDATE: "guessUpdate",
+	RESULTS: "results",
+	TURN_INFO: "turnInfo",
+	DRAWING: "drawing"
+} as const;
+
+export type WSEvent = typeof WS_EVENTS[keyof typeof WS_EVENTS]
+
+/*USAGE
+socker.emit(WS_EVENTS.MACRO_NAME, payload);
+*/
---
+++ requirements/shared/ws.payloads.ts
@@ -0,0 +1,43 @@
+import { PlayerDto } from "./player.dto";
+
+export interface JoinRoomPayload {
+	user_id: number;
+}
+
+export interface TurnInfo {
+	roomd_id: number;
+	drawer: number;
+	word: string | null;
+	word_length: number;
+	round: number;
+	turn: number;
+	players: PlayerDto[];
+}
+
+export interface GuessPayload {
+	guesser_id: number;
+	guess: string;
+	roomd_id: number;
+}
+
+export interface GuessUpdatePayload {
+	guesser_id: number;
+	guess: string | null;
+	roomd_id: number;
+	score: number;
+	correct: boolean;
+}
+
+export interface ResultsPayload {
+	final: boolean;
+	solution: string;
+	time_to_display: number;
+}
+
+export interface DrawingPayload {
+	room_id: number;
+	drawer: Number;
+	coordinate_x: number;
+	coordinate_y: number;
+	color: `#${string}`// e.g. "#ff00ff"
+}
