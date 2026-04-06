046ddcbcf08ef8d64305f8c2517ded112616c9c6
lotte <lieselot.detaeye@gmail.com>
2026-04-06 22:54:24 +0200

    add: scoreboard to postgame page

--- requirements/frontend/src/pages/gameRoom.tsx
+++ requirements/frontend/src/pages/gameRoom.tsx
@@ -315,6 +315,7 @@ export default function GamePage() {
                   solution: payload.solution,
                   winnerText: formatNames(roundWinners),
                   winnerScore: topScore,
+                  players: scoringPlayers,
                 },
               },
             });
